975f415f98ccc61c5691001b0ef6be465bf49964
Shinya Tawata <tawatashinya@gmail.com>
2020-07-07 20:47:16 +0900

    Added root path

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,48 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    # @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.valid?
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  # def edit
+  # end
+
+  # def update
+  #   @cocktail.update(cocktail_params)
+  #   redirect_to cocktail_path(@cocktail)
+  # end
+
+  # def destroy
+  #   @cocktail.destroy
+  #   redirect_to cocktails_path
+  # end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
